4b3430b63be62c5c4cc937ecdee4b5776ff95af7
Paul <118978940+plcr9@users.noreply.github.com>
2024-08-26 06:54:59 +0100

    Update numbers.cs

--- numbers.cs
+++ numbers.cs
@@ -1,1 +1,28 @@
+using Systam;
 
+namespace BusinessSolution
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //calculating net income
+      double revenue = 853023.45;
+      double expenses - 438374.11;
+      double netIncome - revenue - expenses;
+
+      Console.WriteLine(netIncome);
+
+      //calculating a break-even point
+      double fixedCosts = 912849.30;
+      double salesPrice = 29.99;
+      double variableCostPerUnit = 17.65;
+
+      double breakEvenVolume = fixedCosts / (salesPrice - variableCostPerUnit);
+      Console.WriteLine(breakEvenVolume);
+    }
+  }
+}
+
+
+      
