71abc970ca6c2343fcce61f9562b8d380a7cb9e0
Paul <118978940+plcr9@users.noreply.github.com>
2024-08-30 00:34:09 +0100

    Update class_teams.cs

--- class_teams.cs
+++ class_teams.cs
@@ -1,1 +1,17 @@
-c
+using System;
+
+namespace ClassTeams
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int students = 18;
+
+      int groupSize = 3;
+
+      Console.WriteLine(students % groupSize);
+
+    }
+  }
+}
