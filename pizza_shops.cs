1c3132386f7a8ec92422e8cd20336b56a95b0d96
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-01 07:07:48 +0100

    Update pizza_shops.cs

--- pizza_shops.cs
+++ pizza_shops.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace Numbers
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int pizzaShops = 4332;
+
+      int totalEmployees = 86928;
+
+      decimal revenue = 390819.28m;
+
+      Console.WriteLine(pizzashops);
+      Console.WriteLine(totalEmployees);
+      Console.WriteLine(revenue);
+    }
+  }
+}
