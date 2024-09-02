79c3e413fdbffaf71e41ae63080647503e96c3ea
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-02 06:19:56 +0100

    Update arith_op.cs

--- arith_op.cs
+++ arith_op.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace PlanetCalculations
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int userAge = 30;
+
+      double jupiterYears = 11.86;
+
+      double jupiterAge = userAge/jupiterYears;
+
+      double newEarthAge = userAge + journeyToJupiter;
+
+      double newJupiterAge = newEarthAge/jupiterYears;
+
+      Console.WriteLine(jupiterAge);
+      Console.WriteLine(newEarthAge);
+      Console.WriteLine(newJupiterAge);
+    }
+  }
+}
