19bb05d8f499623027a572dd7686ba7e50627c2d
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-04 06:06:12 +0100

    Update get_input.cs

--- get_input.cs
+++ get_input.cs
@@ -1,1 +1,14 @@
+using System;
 
+namespace GettingInput
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("How old are you?");
+      string input = Console.Readline();
+      Console.WriteLine($"You are {input} years old!");
+    }
+  }
+}
