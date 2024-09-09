61acd7df371f38fc5412aab1cebadca56e5dcd2c
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-09 06:11:44 +0100

    Update if_else.cs

--- if_else.cs
+++ if_else.cs
@@ -3,3 +3,12 @@ using System;
 namespace IfElseStatement
 {
   class Program
+  {
+    static void Main(string[] args)
+    {
+      int people = 10;
+      string weather = "nice";
+    }
+  }
+}
+}
