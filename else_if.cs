cf7339e02fcd403ed60fc58661cd86c35eb34af8
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-10 05:58:51 +0100

    Update else_if.cs

--- else_if.cs
+++ else_if.cs
@@ -1,1 +1,26 @@
+using System;
+
+namespace ElseIfStatement
+{
+  class Program
+  {
+    static void Main(string[] orgs)
+    {
+      double ph = 7.0;
+
+      if (ph < 7)
+      {
+        Console.WriteLine("Acidic");
+      }
+      else if (ph > 7)
+      {
+        Console.WriteLine("Basic");
+      }
+      else
+      {
+        Console.WriteLine("Neutral");
+      }
+    }
+  }
+}
 
