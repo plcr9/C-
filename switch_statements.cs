8354fcba599fa817239ce8052140d9ae7e90f796
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-11 06:15:41 +0100

    Update switch_statements.cs

--- switch_statements.cs
+++ switch_statements.cs
@@ -1,1 +1,27 @@
+using System;
 
+namespace SwitchStatement
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double ph = 14;
+
+      switch(ph) {
+        case <= 3:
+          Console.WriteLine("Very Acidic");
+          break;
+        case < 7:
+          Console.WriteLine("Acidic");
+          break;
+        case >= 11:
+          Console.WriteLine("Very Basic");
+          break;
+        default:
+          Console.WriteLine("Neutral");
+          break;
+      }
+    }
+  }
+}
