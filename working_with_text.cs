66fe7a0f5d02a2f3e051ec939a9efbb4e8d140fa
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-13 06:48:10 +0100

    Update working_with_text.cs

--- working_with_text.cs
+++ working_with_text.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace MadTeaParty
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string drink = "wine";
+      string madTeaParty = $"\Have some {drink},\" the March Hare said in an encouraging tone. \nAlice looked all round the table, but ther awas nothing on it but tea. \n\"I don't see any {drink},\" she remarked. \n\"There isn't any,\" said the March Hare.";
+
+      int storyLength = madTeaParty.Length;
+      string toFind = "March Hare";
+
+      string findLowerCase = toFind.ToLower();
+      int findMarchHare = madTeaParty.IndexOf(toFind);
+
+    }
+  }
+}
