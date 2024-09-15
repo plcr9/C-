89709550190307a89686cae6fff5ce52cd21c9d6
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-15 07:04:10 +0100

    Update building_strings.cs

--- building_strings.cs
+++ building_strings.cs
@@ -1,1 +1,18 @@
+using System;
 
+namespace PrideAndPrejudice
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string firstSentence = "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+
+      string firstSpeech = "\"My dear Mr. Bennet,\" said his lady to him one day, \"have you heard that Netherfield Park is let at last?\"";
+
+      Console.WriteLine(firstSentence);
+      Console.WriteLine("\n");
+      Console.WriteLine(firstSpeech);
+    }
+  }
+}
