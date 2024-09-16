09be86c3a051d1693f97903e8a7aac0bb4c89ff9
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-16 02:43:04 +0100

    Update string_concat.cs

--- string_concat.cs
+++ string_concat.cs
@@ -1,1 +1,18 @@
+using System;
 
+namespace StoryTime
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string beginning = "Once upon a time";
+      string middle = "the kid climbed a tree";
+      string end = "Everyone lived happily ever after";
+
+      string story = beginning + ", " + middle + ". " + end ".";
+
+      Console.WriteLine(story);
+    }
+  }
+}
