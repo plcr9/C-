e1f9df8baa489d1f3b2abe04574e88e5e17bf052
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-17 06:42:40 +0100

    Update string_interpolation.cs

--- string_interpolation.cs
+++ string_interpolation.cs
@@ -1,1 +1,18 @@
+using System;
 
+namespace StoryTime
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string beginning = "Once upon a time,";
+      string middle = "The kid climbed a tree";
+      string end = "Everyone lived happily ever after.";
+
+      string story = ${beginning} it was a beautiful day. {middle} and the birds flew through the sky. {end}";
+
+      Console.WriteLine(story);
+    }
+  }
+}
