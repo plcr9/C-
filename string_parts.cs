0d6a9c3689ea57b50a00d831482772a1fac55c87
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-20 05:57:42 +0100

    Update string_parts.cs

--- string_parts.cs
+++ string_parts.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace DNA
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string startStrand = "ATGCGATGAGCTTAC";
+
+      int tgo = startStrand.IndexOf("TGA");
+
+      int startPoint = 0;
+      int length = tgo + 3;
+
+      string dna = startStrand.Substring(startPoint, length);
+      Console.WriteLine(dna);
+
+      Console.WriteLine(dna[3]);
+    }
+  }
+}
