8f3ede4543813169f82da56fd81c43958493d57a
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-21 08:13:57 +0100

    Update string_manipulation.cs

--- string_manipulation.cs
+++ string_manipulation.cs
@@ -1,1 +1,25 @@
+using System;
 
+namespace MovieScript
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string script = "Close on a portrait of the HANDSOME PRINCE -- as the BEAST'S giant paw slashes it.";
+
+      int charPosition = script.IndexOf("Close");
+      int length = "Close on".Length;
+      string cameraDirections = script.Substring(charPosition,length);
+
+      charPosition = script.IndexOf("a portrait");
+      string sceneDescription = script.Substring(charPosition);
+
+      cameraDirections = cameraDirections.ToUpper();
+
+      sceneDescription = sceneDescription.ToLower();
+
+      Console.WriteLine($"{cameraDirections}{sceneDescription}");
+    }
+  }
+}
