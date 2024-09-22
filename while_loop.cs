49cdce1d4752981c5799de6c3dcdbb3f0ef498ed
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-22 11:35:35 +0100

    Update while_loop.cs

--- while_loop.cs
+++ while_loop.cs
@@ -1,1 +1,21 @@
+using System;
 
+namespace WhileLoop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int emails = 20;
+
+      while (emails > 0)
+      {
+        emails--;
+        Console.WriteLine($"one email deleted, {emails} left");
+      }
+
+      Console.WriteLine("INBOX ZERO ACHIEVED!");
+
+    }
+  }
+}
