d9234c97aaf6d3dedc554f077216136ba260054b
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-23 00:19:27 +0100

    Update do_while_loop.cs

--- do_while_loop.cs
+++ do_while_loop.cs
@@ -1,1 +1,19 @@
+using System;
 
+namespace DoWhileLoop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool buttonClick = true;
+
+      do
+      {
+        Console.WriteLine("Alarm Ringing");
+      } while(!buttonClick);
+
+      Console.WriteLine("Time for a five minute break");
+    }
+  }
+}
