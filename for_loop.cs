1bc17f5eedaa670960284c38e232691f1a133b42
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-24 06:47:18 +0100

    Update for_loop.cs

--- for_loop.cs
+++ for_loop.cs
@@ -1,1 +1,16 @@
+using System;
 
+namespace ForLoop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for(int i = 1; i <= 16, i++) {
+        Console.WriteLine($"Week {i}");
+        Console.WriteLine("Announcements: \n \n \n");
+        Console.WriteLine("Report Backs: \n \n \n");
+        Console.WriteLine("Discussion Items: \n \n \n");
+      }
+    }
+  }
