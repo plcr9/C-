c3e2a9945b0b2d9772b7b1cf3119fb03e129f373
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-25 06:34:24 +0100

    Update for_each_loop.cs

--- for_each_loop.cs
+++ for_each_loop.cs
@@ -1,1 +1,17 @@
+using System;
 
+namespace ForEachLoop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] todo = {"respond to email", "make wireframe", "program feature", "fix bugs"};
+
+      foreach (string task in todo)
+      {
+        Console.WriteLine($"[] {task}");
+      }
+    }
+  }
+}
