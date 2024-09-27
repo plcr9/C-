f319b9d4211ddfd0414d8bea5d593cbfa0af6557
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-27 07:09:42 +0100

    Update jump_statements.cs

--- jump_statements.cs
+++ jump_statements.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace JumpStatements
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool buttonClick = false;
+      int counter = 0;
+
+      do
+      {
+        Console.WriteLine("Alarm Ringing";
+        counter++;
+
+        if (counter == 3)
+        {
+          break:
+        }
+      } while(!buttonClick);
+     }
+  }
+}
