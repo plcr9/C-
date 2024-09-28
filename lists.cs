4cd932ef52e61d2530eeb7cb3c04aa04b47c16dc
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-28 08:09:11 +0100

    Update lists.cs

--- lists.cs
+++ lists.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Collection.Generic;
 
+namespace LearnLists
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<string> citiesList = new
+      List<string> { "Delhi", "Los Angeles", "Canberra" };
+
+      citiesList.Add("Naples");
+
+      citiesList.Remove("Delhi");
+
+      citiesList.AddRange(new string[] {"Cairo", "Pretoria"});
+
+      bool hasNewDelhi = citiesList.Contains("New Delhi");
+
+      foreach (string city in citiesList)
+      {
+        Console.WriteLine(city);
+      }
+    }
+  }
+}
