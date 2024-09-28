ea9f33a7b3f4c4a23abbac17531068eafe3f406f
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-29 00:16:30 +0100

    Update creating_and_adding.cs

--- creating_and_adding.cs
+++ creating_and_adding.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Collection.Generic;
 
+namespace LearnLists
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<double> marathons = new List<double>();
+      marathons.Add(144.07);
+      marathons.Add(143,12);
+
+      Consold.WriteLine(marathons[1]);
+    }
+  }
+}
