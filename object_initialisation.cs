04e3cc8d55783a8087664e0f403fb8192eeb7960
Paul <118978940+plcr9@users.noreply.github.com>
2024-09-30 02:22:58 +0100

    Update object_initialisation.cs

--- object_initialisation.cs
+++ object_initialisation.cs
@@ -1,1 +1,23 @@
+using System;
+using System.Collections.Generic;
 
+namespace LearnLists
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<double> marathons = new List<double>
+      {
+        144.07,
+        143.12,
+        146.73,
+        146,33,
+      };
+
+      double time = marathons[1];
+
+      Console.WriteLine($"The 2012 marathon was run in {time} minutes!");
+    }
+  }
+}
