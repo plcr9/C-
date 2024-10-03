5c22b57c4cd8162e33c57de35173f24a43494131
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-03 06:13:13 +0100

    Update clearing.cs

--- clearing.cs
+++ clearing.cs
@@ -1,1 +1,24 @@
+using System;
+using System.Collections.Generic;
 
+namespace LearnLists
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<double> marathons = new List<double>
+      {
+        148.22,
+        156.10,
+        153.88,
+        155.34,
+        160.89
+        };
+
+      Console.WriteLine($"Count: {marathons.Count}");
+
+    }
+  }
+}
+      
