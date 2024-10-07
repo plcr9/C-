19cb166f01b65f6e31e28218e8d96a5110958f89
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-07 07:42:55 +0100

    Update generic_collections.cs

--- generic_collections.cs
+++ generic_collections.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Collections.Generic;
 
+namespace LearnLists
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<bool> b = new List<bool>();
+      List<Random> r = new List<Random>();
+      List<IServiceProvider> i = new
+      List<IServiceProvider>();
+    }
+  }
+}
