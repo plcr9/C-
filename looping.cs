1c45d55ef33b23d3cf0c7405099302b0fc1e8c69
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-08 11:07:19 +0100

    Update looping.cs

--- looping.cs
+++ looping.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Collections.Generic;
 
+namespace LearnLists
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<string> citiesList = new
+      List<string> { "Paris", "Dubai", "Darwin" };
+
+      citiesList.Add("San Francisco");
+
+      foreach (string city in citiesList)
+      {
+        Console.WriteLine($"Welcome to...{city}!");
+      }
+    }
+  }
+}
