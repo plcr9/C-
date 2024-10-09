ba6ec3f9777ecaeffaa40ca76cdd98a0b4b26079
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-09 06:39:04 +0100

    Update calling_methods.cs

--- calling_methods.cs
+++ calling_methods.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace CallingMethods
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string designer = "Anders Hejlsberg";
+
+      Console.WriteLine(designer);
+
+      int indexOfSpace = designer.IndexOf(" ");
+
+      int indexOfSecondName = indexOfSpace + 1;
+
+      string secondName = designer.Substring(indexOfSecondName);
+
+      Console.WriteLine(secondName);
+    }
+  }
+}
