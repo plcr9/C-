dc6e148b3fadc82f265dfc06727bd2b4b4a72c94
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-10 06:35:49 +0100

    Update defining_methods.cs

--- defining_methods.cs
+++ defining_methods.cs
@@ -1,1 +1,17 @@
+using System;
 
+namespace DefineMethod
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      VisitPlanets();
+    }
+
+    static void VisitPlanets()
+    {
+      Console.WriteLine("You have visited new planets...");
+    }
+  }
+}
