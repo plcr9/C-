e6545d38c2774f5f191662cbbb7f96f1ca46ea4e
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-13 00:22:01 +0100

    Update method_overloading.cs

--- method_overloading.cs
+++ method_overloading.cs
@@ -1,1 +1,29 @@
+using System;
 
+namespace MethodOverloading
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      IntroduceFriends("Laika", "Albert");
+      IntroduceFriends("Naomi", "Jasmine", "Cyrus");
+      IntroduceFriends();
+    }
+
+    static void IntroduceFriends(string friend1, string friend2)
+    {
+      Console.WriteLine($"These are my friends, {friend1} and {friend2}!");
+    }
+
+    static void IntroduceFriends(string friend1, string friend2, string friend3)
+    {
+      Console.WriteLine($"These are my friends, {friend1}, {friend2}, and {friend3}!");
+    }
+
+    static void IntroduceFriends()
+    {
+      Console.WriteLine("There is no one who needs to be introduced.");
+    }
+  }
+}
