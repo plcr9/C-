5aa16c8a779a25998c25478bda62c49e53af3447
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-14 08:41:46 +0100

    Update out_parameters.cs

--- out_parameters.cs
+++ out_parameters.cs
@@ -1,1 +1,36 @@
+using System;
 
+namespace OutParameters
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string scoreAsString = "85.6";
+      string statement = "Hello World";
+
+      double scoreAsDouble;
+      bool outcome;
+
+      outcome = Double.TryParse(scoreAsString, out scoreAsDouble);
+
+      Console.WriteLine($"{outcome}, {scoreAsDouble}");
+
+      string whispered = Whisper(statement, out bool marker);
+      Console.WriteLine(whispered);
+    }
+
+    static string Whisper(string phrase, out bool wasWhisperCalled)
+    {
+      wasWhisperCalled = true;
+      return phrase.ToLower();
+    }
+  }
+}
+
+
+
+
+
+
+      
