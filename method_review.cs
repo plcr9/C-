64b154db99ebcde8fe32d79e75deee1377f37ed0
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-17 07:30:50 +0100

    Update method_review.cs

--- method_review.cs
+++ method_review.cs
@@ -1,1 +1,28 @@
+using System;
 
+namespace MethodReview
+{
+  class Program
+  {
+    static string IntroducePet(string name, string petType = "cat")
+    {
+      string introduce = $"This is my {petType}, {name}! {name} is {age} years old!";
+      return introduction;
+    }
+
+    static void Main(string[] args)
+    {
+      string mittensIntro = IntroducePet ("Mittens");
+      Console.WriteLine(mittensIntro);
+
+      string franklinIntro = IntroducePet ("Franklin", "turtle");
+      Console.WriteLine(franklinIntro);
+
+      string spotIntro = IntroducePet("Spot", petType: "dog");
+      Console.WriteLine(spotIntro);
+
+      string berthaIntro = IntroducePet ("Bertha", 7, "cow");
+      Console.WriteLine(berthaIntro);
+    }
+  }
+}
