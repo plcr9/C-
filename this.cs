12b477f95ae2eb79cbfd99f3702cd1cc5a584f6d
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-21 06:29:45 +0100

    Update this.cs

--- this.cs
+++ this.cs
@@ -1,1 +1,26 @@
+using System;
 
+namespace ThisKeyword
+{
+  class Forest
+  {
+    public string namne;
+    public int trees;
+    public int age;
+    public string biome;
+
+    public int Grow()
+    {
+      this.trees += 30;
+      this.age++;
+      return this.trees;
+    }
+
+    public int Burn()
+    {
+      trees -= 20;
+      age++;
+      return trees;
+    }
+  }
+}
