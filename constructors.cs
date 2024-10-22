1fc361dafbbb28c7a871b662313a46790f0b2bbd
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-22 06:42:32 +0100

    Update constructors.cs

--- constructors.cs
+++ constructors.cs
@@ -1,1 +1,26 @@
+using System;
 
+namespace Constructors
+{
+  public class Forest
+  {
+    public string name;
+    public int trees;
+    public int age;
+    public string biome;
+
+    public int Grow()
+    {
+      this.trees += 30;
+      this.age++;
+      return this.trees;
+    }
+
+    public int Burn()
+    {
+      trees -= 20;
+      age++;
+      return trees;
+    }
+  }
+}
