ee7485c639c23629b2b8802d6c39f4508f7a01dc
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-23 07:46:45 +0100

    Update overloading_constructors.cs

--- overloading_constructors.cs
+++ overloading_constructors.cs
@@ -1,1 +1,38 @@
+using System;
 
+namespace OverloadingConstructors
+{
+  class Forest
+  {
+    public string name;
+    public int trees;
+    public int age;
+    public string biome;
+
+    public Forest(string name, string biome)
+    {
+      this.name = name;
+      this.biome = biome;
+      age = 1;
+    }
+
+    public Forest(string name) : this(name, "Unknown")
+    {
+      Console.WriteLine("No biome provided, using default value \"Unknown\".");
+    }
+
+    public int Grow()
+    {
+      this.tree += 30;
+      this.age++;
+      return this.trees;
+    }
+
+    public int Burn()
+    {
+      trees -= 20;
+      age++;
+      return trees;
+    }
+  }
+}
