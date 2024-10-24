09cc5d4e6ac696602c60d17c8953b1ff17effc49
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-24 07:52:50 +0100

    Update abstraction.cs

--- abstraction.cs
+++ abstraction.cs
@@ -1,1 +1,16 @@
-a
+using System;
+
+namespace Review
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest amazon = new Forest("Amazon");
+      Console.WriteLine(amazon.trees);
+
+      amazon.Grow();
+      Console.WriteLine(amazon.trees);
+    }
+  }
+}
