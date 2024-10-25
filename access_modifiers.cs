9e171c0629012f8a6c443ae6b0b139b40f74ddca
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-25 06:31:56 +0100

    Update access_modifiers.cs

--- access_modifiers.cs
+++ access_modifiers.cs
@@ -1,1 +1,30 @@
+using System;
 
+namespace AccessModifiers
+{
+  class Forest
+  {
+    public string name;
+    int trees;
+    int age;
+    public string biome;
+
+    public Forest(string name, string biome)
+    {
+      this.name = name;
+      this.biome = biome;
+      age = 1;
+    }
+
+    public void Grow()
+    {
+      trees += 20;
+      age++;
+    }
+
+    public void AnnounceForest()
+    {
+      Console.WriteLine($"The {name} {biome} Forest is {age} years old and has {trees} trees");
+    }
+  }
+}
