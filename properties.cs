0862901722b0cb663e5c6e4be0a271ba35f66eec
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-26 07:58:25 +0100

    Update properties.cs

--- properties.cs
+++ properties.cs
@@ -1,1 +1,37 @@
-p
+using System;
+
+namespace Properties
+{
+  public class Forest
+  {
+    private string name;
+    public string Name
+    {
+      get {
+        return name;
+      }
+      set {
+        name = value;
+      }
+    }
+
+    public int trees;
+    public int age;
+
+    private string biome;
+    public string Biome
+    {
+      get {
+        return biome;
+      }
+      set {
+        string[] validBiomes = {"Tropical", "Temperate", "Boreal"};
+        if (Array.IndexOf(validBiomes, value) => 0) {
+          biome = value;
+        }
+        else {
+          biome = "Unknown";
+        }
+      }
+    }
+  }
