22ccb419d37730e83b47e27747ebc233d8e55bf9
Paul <118978940+plcr9@users.noreply.github.com>
2024-10-27 07:23:20 +0000

    Update auto_implemented_properties.cs

--- auto_implemented_properties.cs
+++ auto_implemented_properties.cs
@@ -1,1 +1,33 @@
+using System;
 
+namespace AutoImplementedProperties
+{
+  class Forest
+  {
+    public string Name
+    { get; set; }
+
+    public int Trees
+    { get; set; }
+
+    public int age;
+
+    private string biome;
+    public string Biome
+    {
+      get {
+        return biome;
+      }
+
+      set {
+        string[] validBiomes = {"Tropical", "Temperate", "Boreal"};
+        if (Array.IndexOf(validBiomes, value >= 0) {
+          biome = value;
+        }
+        else {
+          biome = "Unknown";
+        }
+      }
+    }
+  }
+}
