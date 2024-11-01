1a5fcd6851d9e61e65f557c29f2ebef63553cdf1
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-01 06:37:25 +0000

    Update static_classes.cs

--- static_classes.cs
+++ static_classes.cs
@@ -1,1 +1,14 @@
+using System;
 
+namespace StaticClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.PI);
+
+      Console.WriteLine(Math.Abs(-32));
+    }
+  }
+}
