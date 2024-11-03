4906be798d776ef29aed79aba5934d0e7239a132
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-03 07:39:28 +0000

    Update intro_to_inheritance.cs

--- intro_to_inheritance.cs
+++ intro_to_inheritance.cs
@@ -1,1 +1,40 @@
+using System;
+
+namespace LearnInheritance
+{
+  class Sedan
+  {
+    public string LicensePlate
+    { get; }
+
+    public double speed
+    { get; private set; }
+
+    public int Wheels
+    { get; }
+
+    public Sedam(double speed)
+    {
+      Speed = speed;
+      LicensePlate = Tools.GenerateLicensePlate();
+      Wheels = 4;
+    }
+
+    public void Honk()
+    {
+      Console.WriteLine("HONK!");
+    }
+
+    public void SpeedUp()
+    {
+      Speed += 5;
+    }
+
+    public void SlowDown()
+    {
+      Speed -= 5;
+    }
+
+  }
+}
 
