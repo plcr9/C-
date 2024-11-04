24348b665674bc4660b68e54bb0b71a4c51a8ad2
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-04 06:27:53 +0000

    Update superclass_creation.cs

--- superclass_creation.cs
+++ superclass_creation.cs
@@ -1,1 +1,31 @@
+using System;
 
+namespace LearnInheritance
+{
+  class Vehicle
+  {
+    public string LicensePlate
+    { get; }
+
+    public double Speed
+    { get; private set; }
+
+    public int Wheels
+    { get; }
+
+    public void SpeedUp()
+    {
+      Speed += 5;
+    }
+
+    public void SlowDown()
+    {
+      Speed -= 5;
+    }
+
+    public void Honk()
+    {
+      Console.WriteLine("HONK!");
+    }
+  }
+}
