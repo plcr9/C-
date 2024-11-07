14501984dc07b88c3f63c351a237de51a5ba1aca
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-07 07:34:49 +0000

    Update access_inherited_members_with_base.cs

--- access_inherited_members_with_base.cs
+++ access_inherited_members_with_base.cs
@@ -1,1 +1,37 @@
+using System;
 
+namespace LearnInheritance
+{
+  class Vehicle
+  {
+    public string LicensePlate
+    { get; private set; }
+
+    public double Speed
+    { get; private set; }
+
+    public int Wheels
+    { get; protected set; }
+
+    public Vehicle(double speed)
+    {
+      Speed = speed;
+      LicensePlate = Tools.GenerateLicensePlate();
+    }
+
+    public void SpeedUp()
+    {
+      Speed += 5;
+    }
+
+    public void SlowDown()
+    {
+      Speed -= 5;
+    }
+
+    public void Honk()
+    {
+      Console.WriteLine("HONK!");
+    }
+  }
+}
