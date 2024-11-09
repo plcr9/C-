3d69171858769df2bb98f3130e41d25e096af942
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-09 07:40:49 +0000

    Update intro_to_polymorphism.cs

--- intro_to_polymorphism.cs
+++ intro_to_polymorphism.cs
@@ -1,1 +1,33 @@
+using System;
+
+namespace PolymorphismBasics
+{
+  class Animal
+  {
+    public virtual void MakeSound()
+    {
+      Console.WriteLine("Animal makes a sound.");
+    }
+  }
+
+  class Dog : Animal
+  {
+    public override void MakeSound()
+    {
+      Console.WriteLine("Dog barks.");
+    }
+  }
+
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      Animal myDog = new Dog();
+      Animal myCat = new Cat();
+
+      myDog.MakeSound();
+      myCat.MakeSound();
+    }
+  }
+}
 
