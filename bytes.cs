cad282c5d49776d57fc8f3d4e37477ae96bc72ef
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-10 07:42:23 +0000

    Update bytes.cs

--- bytes.cs
+++ bytes.cs
@@ -1,1 +1,17 @@
+using System;
 
+public class Program
+{
+	public static void Main()
+	{
+		byte b1 = 255;
+		//byte b2 = -128; // compile-time error: Constant value '-128' cannot be converted to a 'byte'
+		sbyte sb1 = -128;
+		sbyte sb2 = 127;
+
+		Console.WriteLine(Byte.MaxValue);
+		Console.WriteLine(Byte.MinValue);
+		Console.WriteLine(SByte.MaxValue);
+		Console.WriteLine(SByte.MinValue);
+	}
+}
