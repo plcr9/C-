f540df001cdbc40fbc9cb7a1b1c4a89ca26094be
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-11 06:10:17 +0000

    Update throttle.cs

--- throttle.cs
+++ throttle.cs
@@ -1,1 +1,13 @@
+class Car
 
+  public void fullThrottle()
+  {
+    Console.WriteLine("Tha car is going as fast as it can!");
+  }
+
+  static void Main(string[] args)
+  {
+    Car myObj = new Car();
+    myObj.fullThrottle();
+  }
+}
