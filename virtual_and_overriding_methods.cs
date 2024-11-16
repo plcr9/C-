4eed026306827c8c772fc09791fc8024260c114a
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-16 11:51:42 +0000

    Update virtual_and_overriding_methods.cs

--- virtual_and_overriding_methods.cs
+++ virtual_and_overriding_methods.cs
@@ -1,1 +1,37 @@
+using System;
 
+namespace PolymrphismBasics
+{
+  public class Employee
+  {
+    public void MakeHRRequest()
+    {
+      Condole.WriteLine("Employee makes an HR request.");
+    }
+  }
+
+  public class Manager : Employee
+  {
+    public void MakeHRRequest()
+    {
+      Console.WriteLine("Manager makes an HR request.");
+    }
+  }
+
+  public class Engineer : Employee
+  {
+
+  }
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Manager manager1 = new Manager();
+      manager1.MakeHRRequest();
+
+    }
+  }
+}
+
+      
