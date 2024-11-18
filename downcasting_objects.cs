5f49d152e1119029407b1a5465f58dce97fdf40e
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-18 00:12:56 +0000

    Update downcasting_objects.cs

--- downcasting_objects.cs
+++ downcasting_objects.cs
@@ -1,1 +1,35 @@
+using System;
+
+namespace PolymorphismBasics
+{
+  public class Employee
+  {
+    public virtual void MakeHRRequest()
+    {
+      Console.WriteLine("Employee makes an HR request.");
+    }
+  }
+
+  public class Manager : Employee
+  {
+    public override void MakeHRRequest()
+    {
+      Console.WriteLine("Manager makes an HR request.");
+    }
+
+    public void MoveToOffice()
+    {
+      Console.WriteLine("Manager moves to office.");
+    }
+  }
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Employee myEmployeeManager = new Manager();
+    }
+  }
+}
+
 
