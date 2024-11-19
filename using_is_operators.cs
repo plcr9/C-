1ad60981d0761ca06588a216b83be42f6552e8fc
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-19 06:39:43 +0000

    Update using_is_operators.cs

--- using_is_operators.cs
+++ using_is_operators.cs
@@ -1,1 +1,39 @@
+using System;
 
+namespace PolymorphismBasics
+{
+  public class Employee{
+  {
+    public virtual void MakeHRRequest()
+    {
+      Console.WriteLine("Employee makes and HR request.");
+    }
+  }
+
+  public class Engineer : Employee
+  {
+    public override void MakeHRRequest()
+    {
+      Console.WriteLine("Engineer makes an HR request.");
+    }
+  }
+
+  public class Manager : Employee
+  {
+    public override void MakeHRRequest()
+    {
+      Console.WriteLine("Manager makes an HR request.");
+    }
+  }
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Employee myEmployee = new Engineer();
+      Console.WriteLine(myEmployee is Engineer);
+      Console.WriteLine(myEmployee is Manager);
+    }
+  }
+}
+    
