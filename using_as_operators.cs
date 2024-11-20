0c52f00babbd44e5dc3fcf177f22ee352cce250c
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-20 12:00:18 +0000

    Update using_as_operators.cs

--- using_as_operators.cs
+++ using_as_operators.cs
@@ -1,1 +1,54 @@
+using System;
 
+namespace PolymorphismBasics
+{
+  public class Employee
+  {
+    public virtual void MakeHRRequest()
+    {
+      Console.WriteLine("Employee makes an HR request.");
+    }
+  }
+
+  public class Engineer : Engineer
+  {
+    public override void MakeHRRequest()
+    {
+      Console.WriteLine("Engineer makes and HR request.");
+    }
+
+    public void SubmitPR()
+    {
+      Console.WriteLine("Engineer submits a pull request.");
+    }
+  }
+
+  public class Manager : Employee
+  {
+    public override void MakeHRRequest()
+    {
+      Console.WriteLine("Manager makes an HR request.");
+    }
+
+    public void MoveToOffice()
+    {
+      Console.WriteLine("Manager moves to office.");
+    }
+  }
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Employee myEmployeeEngineer1 = new Engineer();
+      Employee myEmployeeEngineer2 = new Engineer();
+      Engineer engineer = myEmployeeEngineer1 as Engineer;
+      Console.WriteLine(engineer == null);
+      Manager manager = myEmployeeEngineer2 as Manager;
+      Console.WriteLine(manager == null);
+    }
+  }
+}
+
+  
+      
