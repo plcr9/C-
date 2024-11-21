8a296fa9f8aa096c26aed41af47f4f9a1ba81fed
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-21 08:07:37 +0000

    Update polymorphism_and_abstract_classes.cs

--- polymorphism_and_abstract_classes.cs
+++ polymorphism_and_abstract_classes.cs
@@ -1,1 +1,34 @@
+using System;
 
+namespace PolymorphismBasics
+{
+  public abstract class Employee
+  {
+    public abstract void MakeHRRequest();
+
+    public void Promotion()
+    {
+      Console.WriteLine("Employee gets a promotion.");
+    }
+  }
+
+  public class Manager : Employee
+  {
+    public override void MakeHRRequest()
+    {
+      Console.WriteLine("Manager makes an HR request.");
+    }
+  }
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Manager manager = new Manager();
+      manager.MakeHRRequest();
+      manager.Promotion();
+    }
+  }
+}
+
+  
