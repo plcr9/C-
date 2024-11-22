83c66416b68fd95569d0eaed4bf34eb7723fb95a
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-22 07:02:12 +0000

    Update polymorphism_summary.cs

--- polymorphism_summary.cs
+++ polymorphism_summary.cs
@@ -1,1 +1,32 @@
+using System;
 
+namespace PolymorphismBasics
+{
+  abstract class Employee
+  {
+    public abstract void MakeHRRequest();
+
+    public void ClockIn()
+    {
+      Console.WriteLine("Employee clocks in.");
+    }
+  }
+
+  class Engineer : Employee
+  {
+    public override void MakeHRRequest()
+    {
+      Console.WriteLine("Engineer makes an HR Request.");
+    }
+
+    public void Promotion()
+    {
+      Console.WriteLine("Engineer gets a promotion.");
+    }
+
+    public void SubmitPR()
+    {
+      Console.WriteLine("Engineer submits a pull request.");
+    }
+  }
+}
