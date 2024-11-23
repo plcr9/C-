3dadd9422fb3e07997a4090e9c4ec16901e7e017
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-23 11:38:13 +0000

    Update intro_to_interfaces.cs

--- intro_to_interfaces.cs
+++ intro_to_interfaces.cs
@@ -1,1 +1,14 @@
+using System;
 
+namespace LearnInterfaces
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      sedan s = new Sedan(60);
+      Console.WriteLine($"Sedan with license plate {s.LicensePlate} and {s.Wheels} wheels, driving at {s.Speed} km/h.");
+      s.Honk();
+    }
+  }
+}
