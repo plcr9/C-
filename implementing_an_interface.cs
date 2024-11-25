3b56ea4bdc8eb20bc1436cb3c726d1a0637efbf3
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-25 10:14:10 +0000

    Update implementing_an_interface.cs

--- implementing_an_interface.cs
+++ implementing_an_interface.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace LearnInterfaces
+{
+  class Sedan : IAutomobile
+  {
+    public string LicensePlate
+    { get; }
+
+    public double speed
+    { get; }
+
+    public int Wheels
+    { get; }
+
+    public void Honk()
+    {
+      Console.WriteLine("HONK!");
+    }
+
+  }
+}
