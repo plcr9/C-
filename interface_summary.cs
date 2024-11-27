871b67380f5998cfcb7d3e42aadf0a0f3b0beb2c
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-27 06:52:54 +0000

    Update interface_summary.cs

--- interface_summary.cs
+++ interface_summary.cs
@@ -1,1 +1,12 @@
+using System;
 
+namespace LearnInterfaces
+{
+  interface IAutomobile
+  {
+    string LicensePlate { get; }
+    double Speed { get; }
+    int Wheels { get; }
+    void Honk();
+  }
+}
