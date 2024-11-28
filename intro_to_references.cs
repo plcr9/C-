27840e30cd6b182951cd6e27d37416152f27ddfa
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-28 07:37:18 +0000

    Update intro_to_references.cs

--- intro_to_references.cs
+++ intro_to_references.cs
@@ -1,1 +1,18 @@
+using System;
 
+namespace LearnReferences
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Encyclopedia enc1 = new
+      Encyclopedia();
+      Encyclopedia enc2 = enc1;
+      enc1.CurrentPage = 0;
+      enc2.CurrentPage = 16;
+      Console.WriteLine(enc1.CurrentPage);
+      Console.WriteLine(enc2.CurrentPage);
+    }
+  }
+}
