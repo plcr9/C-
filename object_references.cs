9f417399e1a1b79985883e89109c1181243007dc
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-29 07:03:30 +0000

    Update object_references.cs

--- object_references.cs
+++ object_references.cs
@@ -1,1 +1,17 @@
+using System;
 
+namespace LearnReferences
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Novel nov1 = new Novel(5);
+      Novel nov2 = nov1;
+      nov2.Flip();
+
+      Console.WriteLine(nov1.CurrentPage);
+      Console.WriteLine(nov2.CurrentPage);
+    }
+  }
+}
