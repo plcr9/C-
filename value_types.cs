2030e867dc9332298cce915c55f827e655cb4b86
Paul <118978940+plcr9@users.noreply.github.com>
2024-11-30 08:20:00 +0000

    Update value_types.cs

--- value_types.cs
+++ value_types.cs
@@ -1,1 +1,18 @@
+using System;
 
+namespace LearnReferences
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool boolValue1 = false;
+      bool boolValue2 = boolvalue1;
+
+      boolValue2 = true;
+
+      Console.WriteLine(boolValue1);
+      Console.WriteLine(boolValue2);
+    }
+  }
+}
