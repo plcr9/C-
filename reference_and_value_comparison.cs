32e1566fe32e0e493b0b6b10fb5dda4cf62fbc8e
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-01 08:57:34 +0000

    Update reference_and_value_comparison.cs

--- reference_and_value_comparison.cs
+++ reference_and_value_comparison.cs
@@ -1,1 +1,18 @@
+using System;
+
+namespace LearnReferences
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Book b1 = new Book();
+      Book b2 = new Book();
+      Book b3 = b1;
+
+      Console.WriteLine(b1 == b2);
+      Console.WriteLine(b1 == b3);
+    }
+  }
+}
 
