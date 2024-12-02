76ba346cf2584cc804330780b6de8cf6d6174a00
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-02 11:42:44 +0000

    Update references_of_different_types.cs

--- references_of_different_types.cs
+++ references_of_different_types.cs
@@ -1,1 +1,18 @@
+using System;
 
+namespace LearnReferences
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Encyclopedia enc = new Encyclopedia();
+      IFlippable fEnc = enc;
+      Book bEnc = enc;
+      fEnc.CurrentPage = 42;
+      bEnc.Stringify();
+      Console.WriteLine(fEnc == bEnc);
+    }
+  }
+}
+    
