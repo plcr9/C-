cb39a16a0ea0f615c1951911ac4d810a74d29e9f
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-03 09:59:37 +0000

    Update arrays_of_references.cs

--- arrays_of_references.cs
+++ arrays_of_references.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace LearnReferences
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Encyclopedia enc1 = new Encyclopedia(32, "Encyclopedia Britannica", "Encyclopedia Britannica");
+      Encyclopedia enc2 = new Encyclopedia(19, "Codecademy Curriculum Team", "Codecademy Encyclopedia of Coding Knowledge");
+      Novel nov1 = new Novel(48, "Jane Austen", "Pride and Prejudice");
+      Novel nov2 = new Novel(23, "Charles Dickens");
+
+      Book[] books = [enc1, enc2, nov1, nov2];
+
+      foreach (Book b in books) {
+        Console.WriteLine(b.Title);
+      }
+    }
+  }
+}
+        
