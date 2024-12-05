bd42d52b9333d97624f824ae60f92d5a8ffe1d79
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-05 06:39:48 +0000

    Update the_object_type.cs

--- the_object_type.cs
+++ the_object_type.cs
@@ -1,1 +1,21 @@
+using System;
+
+namespace TheObjectClass
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Book bk = new Book();
+      Object ok = bk;
+
+      Novel nl = new Novel(38);
+      Object ol = ml;
+
+      int i = 9;
+      Object o = i;
+
+    }
+  }
+}
 
