42cc8ff6855db5d395bbf1b8d4356073d670673a
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-06 06:06:37 +0000

    Update object_members.cs

--- object_members.cs
+++ object_members.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace TheObjectClass
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Book b = new Book();
+      Novel n = new Novel(38);
+      Random r = new Random();
+      int i = 9;
+
+      Object[] objects = { b, n, r, i };
+
+      foreach (Object obj in objects)
+      {
+        Console.WriteLine(obj.GetType());
+      }
+    }
+  }
+}
