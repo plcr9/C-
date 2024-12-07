56dc0c8ef55a3260f68a570e1487a0c9760da679
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-07 21:53:51 +0000

    Update overriding_object_members.cs

--- overriding_object_members.cs
+++ overriding_object_members.cs
@@ -1,1 +1,23 @@
+namespace TheObjectClass
+{
+  class Book
+  {
+    public string Author
+    { get; private set; }
 
+    public string Title
+    { get; private set; }
+
+    public Book(string author = "Unknown", string title = "untitled")
+    {
+      Author = author;
+      Title = title;
+    }
+
+    public virtual string Stringify()
+    {
+      return $"This Book is {Title}, by {Author}.";
+    }
+  }
+}
+      
