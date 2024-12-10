0bb8fa53202f9d0c1bf34c2ad965be2d84df41b5
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-10 09:40:53 +0000

    Update z_intro_to_strings.cs

--- z_intro_to_strings.cs
+++ z_intro_to_strings.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace StringTheException
+{
+  class Program
+  {
+    static voic Main(string[] args)
+    {
+      string dog = "chihuahua";
+      string tinyDog = dog;
+      dog = "dalmation";
+      Console.WriteLine(dog);
+      Console.WriteLine(tinyDog);
+
+      string s1 = "Hello";
+      string s2 = s1;
+      s1 += "World";
+      System.Console.WriteLine(s1);
+      System.Console.WriteLine(s2);
+    }
+  }
+}
