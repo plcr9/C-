6df8e559390b799d6be1073e63303d12f03a9c8c
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-11 06:42:59 +0000

    Update string_act_like_values.cs

--- string_act_like_values.cs
+++ string_act_like_values.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace StringTheException
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s1 = "immutable";
+      string s2 = "immutable";
+
+      Console.WriteLine(s1 == s2);
+
+      Object o1 = new Object();
+      Object o2 = new Object();
+
+      Console.WriteLine(o1 == o2);
+    }
+  }
+}
