fa8f088bfb8d3b544cdd1b2cb4903eef82d98bdf
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-12 07:42:47 +0000

    Update strings_null_empty_unassigned.cs

--- strings_null_empty_unassigned.cs
+++ strings_null_empty_unassigned.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace StringTheException
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Enter some input please: ");
+      string input = Console.ReadLine();
+
+      if (String.IsNullOrEmpty(input))
+      {
+        Console.WriteLine("You didn't enter anything!");
+      }
+      else
+      {
+        Console.WriteLine("Thank you for your submission!");
+      }
+    }
+  }
+}
