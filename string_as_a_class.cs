8604df1d38fc98ddfd331fe5571d6a34471a8e9c
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-13 06:35:29 +0000

    Update string_as_a_class.cs

--- string_as_a_class.cs
+++ string_as_a_class.cs
@@ -1,1 +1,21 @@
+using System;
+
+namespace StringTheException
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string dates = 
+        "January 4th, 2024\n" +
+        "March 25th, 2024\n" +
+        "July 8th, 2024\n" +
+        "December 11th, 2024";
+
+      dates = dates.Replace("th", "");
+
+      Console.WriteLine(dates);
+    }
+  }
+}
 
