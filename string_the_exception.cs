0934d233fac64188bb3d17a9d6d28f9365dd7816
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-14 11:28:24 +0000

    Update string_the_exception.cs

--- string_the_exception.cs
+++ string_the_exception.cs
@@ -1,1 +1,22 @@
+using System;
 
+namespace StringTheException
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string dog = "chihuahua";
+      string tinyDog = dog;
+      dog = "dalmation";
+      Console.WriteLine(dog);
+      Console.WriteLine(tinyDog);
+
+      string s1 = "Hello";
+      string s2 = s1;
+      s1 += "World";
+      System.Console.WriteLine(s1);
+      System.Console.WriteLine(s2);
+    }
+  }
+}
