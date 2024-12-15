0ed05ba0771b4da4fd84d312f121889f6dd621aa
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-15 13:05:39 +0000

    Update introduction_to_LINQ.cs

--- introduction_to_LINQ.cs
+++ introduction_to_LINQ.cs
@@ -1,1 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearnLinq
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<string> heroes = new
+      List<string> { "Astra", "Zephyr", "Nyx", "Orion", "Nova", "Thorne" };
+
+      List<string> longLoudHeroes = new List<string>();
+      
+      foreach (string hero in heroes)
+      {
+        if (hero.Length > 5)
+        {
+          string formatted = hero.ToUpper();
+          longLoudHeroes.Add(formatted);
+        }
+      }
+
+      var longLoudHeroes2 = from h in heroes
+        where h.length > 5
+        select h.ToUpper();
+
+      Console.WriteLine("Your long loud heroes are...");
+
+      foreach (string hero in longLoudHeroes2)
+      {
+        Console.WriteLine(hero);
+      }
+    }
+  }
+}
 
