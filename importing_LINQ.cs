c242e424248fd86fce924ee661151854c4588516
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-16 06:58:21 +0000

    Update importing_LINQ.cs

--- importing_LINQ.cs
+++ importing_LINQ.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace LearnLinq
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<string> heroes = new List<string> { "Zoe", "Liam", "Taryn", "Dorian", "Everett", "Marlena" };
+
+      var shortHeroes = from h in heroes
+                        where h.Length < 5
+                        select h;
+
+      Console.WriteLine("Your short heroes are...");
+
+      foreach (string hero in shortHeroes)
+      {
+        Console.WriteLine(hero);
+      }
+    }
+  }
+}
