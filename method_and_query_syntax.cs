a58c8849a13aa2817358feda551360e38498de1b
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-18 06:30:54 +0000

    Update method_and_query_syntax.cs

--- method_and_query_syntax.cs
+++ method_and_query_syntax.cs
@@ -1,1 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace LearnLinq
+{
+  class Program
+  {
+    static void Main()
+    {
+      string[] heroes = { "Zoe", "Liam", "Taryn", "Dorian", "Everett", "Marlena" };
+
+      var queryResult = from x in heroes
+                        where x.Contains("a")
+                        select $"{x} contains an 'a'";
+
+      var methodResult = heroes
+        .Where(x => x.Contains("a"))
+        .Select(x => $"{x} contains an 'a'");
+
+      Console.WriteLine("queryResult:");
+      foreach (string s in queryResult)
+      {
+        Console.WriteLine(s);
+      }
+
+      Console.WriteLine("\methodResult:");
+      foreach (string s in methodResult)
+      {
+        Console.WriteLine(s);
+      }
+    }
+  }
+}
+      
+
+      
+
+      
