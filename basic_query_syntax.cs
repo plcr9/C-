ecce1a5cac93864e7125c1bf1ab78b4283fb96df
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-19 06:11:55 +0000

    Update basic_query_syntax.cs

--- basic_query_syntax.cs
+++ basic_query_syntax.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace LearnLinq
+{
+  class Program
+  {
+    static void Main()
+    {
+      string[] heroes = { "Zoe", "Liam", "Taryn", "Dorian", "Everett", "Marlena" };
+
+      var heroesWithI = from hero in heroes
+        where hero.Contains("i")
+        select hero;
+
+      var understood = from hero in heroes
+        select hero.Replace('a', '_');
+
+    }
+  }
+}
