80d54e61d4f0fa121476e0c609b3419f429b80b4
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-20 06:49:42 +0000

    Update basic_method_syntax_where.cs

--- basic_method_syntax_where.cs
+++ basic_method_syntax_where.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearnLinq
+{
+  class Program
+  {
+    static void Main()
+    {
+      string[] heroes = { "Zoe", "Liam", "Taryn", "Dorian", "Everett", "Marlena" };
+
+      var heroesWithI = heroes.Where(h => h.Contains("i"));
+
+      foreach (var hero in heroeswithI)
+      {
+        Console.WriteLine(hero);
+      }
+    }
+  }
+}
 
