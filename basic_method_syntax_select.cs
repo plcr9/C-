c8b92967510748ef381afdefce104496c2bf761e
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-21 12:06:10 +0000

    Update basic_method_syntax_select.cs

--- basic_method_syntax_select.cs
+++ basic_method_syntax_select.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearnLinq
+{
+  class Program
+  {
+    static void Main()
+    {
+      string[] heroes = { "Zoe", "Liam", "Taryn", "Dorian", "Everett", "Marlena" };
+
+      var heroesWithR = heroes.Where(h => h.Contains("r"));
+      var lowerHeroesWithR = heroesWithR.Select(h => h.ToLower());
+
+      var sameResult = heroes
+        .Select(h => h.ToLower())
+        .Where(h => h.Contains("r"));
+
+      foreach (var hero in sameResult)
+      {
+        Console.WriteLine(hero);
+      }
+    }
+  }
+}
 
