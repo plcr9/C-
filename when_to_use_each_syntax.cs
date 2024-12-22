479879ba23cc4658dd5fb49c2ede8a841654ee5e
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-22 07:25:17 +0000

    Update when_to_use_each_syntax.cs

--- when_to_use_each_syntax.cs
+++ when_to_use_each_syntax.cs
@@ -1,1 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace LearnLinq
+{
+  class Program
+  {
+    static void Main()
+    {
+      string[] heroes = { "Zoe", "Liam", "Taryn", "Dorian", "Everett", "Marlena" };
+
+      var result = heroes.select(h => $"Introducing...{h}!");
+
+      var result2 = from h in heroes
+        where h.Contains("i")
+        select h.IndexOf("i")
+
+      Console.WriteLine("'result': ");
+      foreach (var v in result)
+      {
+        Console.WriteLine(v);
+      }
+
+      Console.WriteLine("\n'result2': ");
+      foreach (var v in result2)
+      {
+        Console.WriteLine(v);
+      }
+    }
+  }
+}
+
+      
