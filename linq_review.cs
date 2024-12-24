e541e51ad63a7475a8dc2da9a115eee7cbc9e141
Paul <118978940+plcr9@users.noreply.github.com>
2024-12-24 13:29:15 +0000

    Update linq_review.cs

--- linq_review.cs
+++ linq_review.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace LearnLinq
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<string> heroesList = new List<string> { "Zoe", "Liam", "Taryn", "Dorian", "Everett", "Marlena" };
+
+      var queryResult = from h in heroesList
+        where h.Contains("a")
+        select $"{h} contains an 'a'";
+
+      Console.WriteLine($"queryResult has {queryResult.Count()} elements");
+
+      foreach (string s in queryResult)
+      {
+        Console.WriteLine(s);
+      }
+    }
+  }
+}
